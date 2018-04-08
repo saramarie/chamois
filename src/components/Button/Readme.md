4d3eb33a4402db550fd54c3749e106287cdbf485
Sara Williams <admin@truthyfalsey.com>
2018-04-07 20:23:43 -0400

    Adding secondary and ghost buttons

--- src/components/Button/Readme.md
+++ src/components/Button/Readme.md
@@ -1,3 +1,17 @@
 ```js
 <Button>Button Text</Button>
 ```
+
+```js
+<Button secondary>Button Text</Button>
+```
+
+```js
+<Button ghost>Button Text</Button>
+```
+
+```js
+<Button secondary ghost>
+  Button Text
+</Button>
+```
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import 'typeface-open-sans'
+import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+import 'typeface-open-sans';
 
-import './styles.css'
+import './styles.css';
+
+const buttonClasses = props =>
+  cn('Button', {
+    'Button--secondary': props.secondary,
+    'Button--ghost': props.ghost,
+  });
 
 const Button = props => (
-  <button type="button" className="Button">
+  <button type="button" className={buttonClasses(props)}>
     {props.text || props.children}
   </button>
-)
+);
 
 Button.propTypes = {
+  type: PropTypes.string,
   text: PropTypes.string,
   children: PropTypes.node,
-}
+  secondary: PropTypes.bool,
+  ghost: PropTypes.bool,
+};
 
-export default Button
+Button.defaultProps = {
+  type: 'text',
+  secondary: false,
+  ghost: false,
+};
+
+export default Button;
