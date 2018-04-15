7a63a981032f11f78dbfabab0c44fac53d7f5527
Sara Williams <admin@truthyfalsey.com>
2018-04-14 20:47:30 -0400

    Fixing hard-coded columns number for article previews

--- src/components/ArticlePreviewSection/index.jsx
+++ src/components/ArticlePreviewSection/index.jsx
@@ -14,7 +14,7 @@ const ArticlePreviewSection = ({ articlePreviews, columns }) => (
     <div className="ArticlePreviewSection-articles">
       <Grid>
         {articlePreviews.map(preview => (
-          <Grid item largeCols="4">
+          <Grid item largeCols={12 / columns}>
             <Card>
               <ArticlePreview
                 datePublished={preview.datePublished}
