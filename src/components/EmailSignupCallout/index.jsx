6a65560c2c66818d7363b1513f7264ecd845f657
Sara Williams <admin@truthyfalsey.com>
2018-04-15 10:43:50 -0400

    Created typography partial

--- src/components/EmailSignupCallout/index.jsx
+++ src/components/EmailSignupCallout/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import PageBlock from '../PageBlock';
 import EmailSignup from '../EmailSignup';
-import './styles.css';
 
 const EmailSignupCallout = props => (
   <PageBlock background="gray">
